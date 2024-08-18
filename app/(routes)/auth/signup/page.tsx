8465a0061d2d120804ba1f4266a89e0c5eea08d2
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-08-18 17:55:38 +0530

    SignupForm Fixed

--- app/(routes)/auth/signup/page.tsx
+++ app/(routes)/auth/signup/page.tsx
@@ -10,7 +10,6 @@ import Button from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -20,14 +19,17 @@ import {
 import { Input } from "@/components/ui/input";
 import { RegisterUser } from "@/actions/auth/register";
 import { OnClick } from "@/actions/auth/socialLogin";
+
 const Signup = () => {
   const form = useForm<z.infer<typeof RegisterSchema>>({
     resolver: zodResolver(RegisterSchema),
     defaultValues: {
       username: "",
+      email: "",
       password: "",
     },
   });
+
   function onSubmit(values: z.infer<typeof RegisterSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
@@ -35,73 +37,98 @@ const Signup = () => {
     console.log(values);
     RegisterUser(values);
   }
+
   return (
-    <div className="p-4">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="username" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="example@domain.com" {...field} />
-                </FormControl>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-[30%] flex flex-col justify-center items-center border-2 border-[#da9cf067]">
+        <div className="p-10">
+          <video preload="none" width="360" height="240" className="rounded-3xl border-2 p-2 border-black" autoPlay loop muted>
+            <source src="vklogo.webm" type="video/webm" />
 
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input type="password" placeholder="password" {...field} />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-      <Button
-        className="my-4"
-        onClick={() => {
-          OnClick("google");
-        }}
-      >
-        <p> google</p>
-      </Button>
-      <Button
-        className="m-4"
-        onClick={() => {
-          OnClick("twitter");
-        }}
-      >
-        <p> twitter </p>
-      </Button>
+            Your browser does not support the video tag.
+          </video>
+          {/* <Image src="/vk.png" height={100} width={100}></Image> */}
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 w-full">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl p-2">Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="username"
+                      {...field}
+                      className="border-[#c84dd6] focus:border-[#76287E] focus:ring-[#76287E] text-xl h-[4h] p-4"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl p-2">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="example@domain.com"
+                      {...field}
+                      className="border-[#c84dd6] focus:border-[#76287E] focus:ring-[#76287E] text-xl h-[4h] p-4"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl p-2">Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="password"
+                      {...field}
+                      className="border-[#c84dd6] focus:border-[#76287E] focus:ring-[#76287E] text-xl h-[4h] p-4"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              type="submit"
+              className="w-full bg-[#76287E] focus:border-[#76287E] focus:ring-[#76287E] text-xl h-[4h] p-4 rounded-md"
+            >
+              Sign Up
+            </Button>
+          </form>
+        </Form>
+        <div className="w-full h-[1px] bg-gradient-to-r from-transparent via-black to-transparent my-5"></div>
+        <button
+          className="relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-[0.5rem] font-medium shadow-input bg-size-200 bg-pos-0 dark:bg-pos-0 hover:bg-pos-100 dark:hover:bg-pos-100 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)] bg-gradient-to-r from-gray-100 via-red-300 to-blue-300 dark:bg-gradient-to-r dark:from-zinc-800 dark:via-red-500 dark:to-blue-500 transition-all duration-300 ease-in-out h-[4h] p-4 text-xl"
+          onClick={() => {
+            OnClick("google");
+          }}
+        >
+          <p>Continue with Google</p>
+        </button>
+        <button
+          className="relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-[0.5rem] font-medium shadow-input bg-size-200 bg-pos-0 dark:bg-pos-0 hover:bg-pos-100 dark:hover:bg-pos-100 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)] bg-gradient-to-r from-blue-100 via-cyan-400 to-cyan-100 dark:bg-gradient-to-r dark:from-zinc-800 dark:via-red-500 dark:to-blue-500 transition-all duration-300 ease-in-out h-[4h] p-4 text-xl mt-4"
+          onClick={() => {
+            OnClick("twitter");
+          }}
+        >
+          <p>Continue with Twitter</p>
+        </button>
+      </div>
     </div>
   );
 };
